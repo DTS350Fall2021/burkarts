b075b47d42ca55088cb815ccffe46af8d1dff830
SamBurkart15 <burkarts.19@william.jewell.edu>
2021-09-14 09:36:55 -0500

    Case study 3 complete

--- Week3/CaseStudy3/CaseStudy3.R
+++ Week3/CaseStudy3/CaseStudy3.R
@@ -10,19 +10,26 @@ life_data <- gapminder %>%
 view(life_data)
 
 #Recreate graphics
-ggplot(data = life_data) +
+plot1 <- ggplot(data = life_data) +
   scale_y_continuous(trans = "sqrt") +
-  geom_point(mapping = aes(size = pop, x = lifeExp, y = gdpPercap, color = continent)) +
+  geom_point(mapping = aes(size = pop/100000, x = lifeExp, y = gdpPercap, color = continent)) +
   facet_wrap(~ year, nrow = 1) +
+  scale_size_continuous(name = "Population (100k)") +
   theme_bw() 
+plot1
 
-ggplot(data = life_data) +
-  scale_y_continuous(trans = "sqrt") +
+weighted_gdpPercap <-  life_data %>%
+  group_by(year, continent) %>%
+  summarise(gdpPercap_weighted = weighted.mean(gdpPercap),
+            Population=pop/10000)
+
+plot2 <- ggplot(data = life_data) +
   geom_point(mapping = aes(x = year, y = gdpPercap, color = continent)) +
   geom_line(mapping = aes(x = year, y = gdpPercap, color = continent, group = country )) +
+  geom_point(data=weighted_gdpPercap, mapping = aes(x = year, y = gdpPercap_weighted, size = Population)) +
+  geom_line(data=weighted_gdpPercap, mapping = aes(x = year, y = gdpPercap_weighted)) +
   facet_wrap(~ continent, nrow = 1) +
   xlab("Year") + ylab("GDP per capita") +
-  summarise(Weighted_Mean = weighted.mean(year, gdpPercap, na.rm = TRUE),
-                                          gdpPercap = sum(continent, na.rm = TRUE))
+  scale_size_continuous(breaks = c(10000, 20000, 30000)) +
   theme_bw() 
-
+plot2
